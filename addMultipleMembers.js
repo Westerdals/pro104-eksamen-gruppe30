124329253e7899212723fbd7fa0ff7d43823e8e6
Markus-Kristiansen <markuskri96@gmail.com>
2020-05-13 19:10:29 +0200

    Lagt til et sokefelt, og har provd aa legge til flere brukere paa en oppgave.

--- addMultipleMembers.js
+++ addMultipleMembers.js
@@ -1,3 +1,10 @@
+/**
+ * Legge til flere teammedlem på en oppgave.
+ * 
+ * Når man opretter en oppgave ønsker vi å kunne legge til flere medlemmer på samme oppgave.
+ * 
+ */
+
 function revealAddTaskSection() {
     var addTaskRevealBtn = document.getElementById("addTaskRevealBtn");
     var addTaskSection = document.getElementById("addTaskSection");
@@ -36,36 +43,45 @@ function revealAddTaskSection() {
     }
   }
 
-  function resetSection(section){
-    section.style.height = "0px";
-    section.style.opacity = "0";
-    if (section == addTaskSection){
-        addTaskRevealBtn.style.backgroundColor = "";
-    } else {
-        addMemberRevealBtn.style.backgroundColor = "";
-    }
-  }
 
-  
-  
 
-  function createTask(event) {
+function createTask(event){
     event.preventDefault();
 
-    const taskName = document.querySelector("[name='taskName']").value;
-    const taskDescription = document.querySelector("[name='taskDescription']").value;
+        //const memberInput = document.querySelector("#memberInput").value;
+        const taskName = document.querySelector("#taskName").value;
+        const taskDescription = document.querySelector("#taskDescription").value;
 
+        
+        const taskList = JSON.parse(localStorage.getItem('task')) || [];
+        const task = {taskName, taskDescription};
+        taskList.push(task);
 
-    const task = {taskName, taskDescription};
-    const taskList = JSON.parse(localStorage.getItem('task')) || [];
-    taskList.push(task);
+        window.localStorage.setItem('task', JSON.stringify(taskList));
+    
+    event.target.reset();
+    renderTaskList();
 
-    window.localStorage.setItem('task', JSON.stringify(taskList));
+}
+
+function createMember(event){
+    event.preventDefault();
+
+    const teamMember = document.querySelector("#teamMember").value;
+
+    const memberList = JSON.parse(localStorage.getItem('member')) || [];
+
+    const member = {teamMember};
+    
+    memberList.push(member);
+
+    window.localStorage.setItem('member', JSON.stringify(memberList));
 
     event.target.reset();
-    resetSection(addTaskSection);
-    renderTaskList();
+    //renderMemberList();
+
 }
+
 
 function renderTaskList() {
 
@@ -85,3 +101,44 @@ function renderTaskList() {
         unstartedTasks.appendChild(taskElement);
     }
 }
+
+function renderMemberList() {
+
+    const memberList = JSON.parse(window.localStorage.getItem("member")) || [];
+    const dropTxt = document.getElementById("droptxt");
+
+    dropTxt.innerHTML = "";
+
+    for (const member of memberList) {
+        dropTxt.innerHTML += `
+                            <p>${member.teamMember}<p>
+                            `;
+    }
+
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+function filterFunction() {
+    renderMemberList();
+    var input, filter, p, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    p = div.getElementsByTagName("p");
+    for (i = 0; i < p.length; i++) {
+      txtValue = p[i].textContent || p[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        p[i].style.display = "";
+      } else {
+        p[i].style.display = "none";
+      }
+    }
+  }
+
+  window.onload = filterFunction;
+
+
+
