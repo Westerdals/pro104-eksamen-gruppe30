48305fbfd5468501bcfb50d2f7b822254244ecd4
furkan2503 <56083285+furkan2503@users.noreply.github.com>
2020-05-20 01:30:19 +0200

    Lagt til checkliste uten css redigering

--- task-manager.js
+++ task-manager.js
@@ -75,8 +75,8 @@ function renderMemberList() {
         membersSection.appendChild(memberElement);
     }
 }
-
-function createChecklistPoint() {
+//Dette er det markus gjorde
+/*function createChecklistPoint() {
 
     var li = document.createElement("li");
     var checklistInput = document.getElementById("taskChecklistInput").value;
@@ -84,5 +84,66 @@ function createChecklistPoint() {
     li.appendChild(node);
 
     document.getElementById("taskChecklist").appendChild(li);
-    document.getElementById("taskChecklistInput").value = "";
-}
+    document.getElementById("taskChecklistInput").value = "";}*/
+    
+    // Create a "close" button and append it to each list item
+    var myNodeList = document.getElementsByTagName("li");
+    var i;
+    for(i = 0; i < myNodeList.length; i++) {
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        myNodeList[i].appendChild(span);
+    }
+
+    // Click on a close button to hide the current list item
+    var close = document.getElementsByClassName("close");
+    var i;
+    for(i = 0; i < close.length; i++){
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+
+    // Add a "checked" symbol when clicking on a list item
+    var list = document.querySelector('ul');
+    list.addEventListener('click', function(ev){
+        if(ev.target.tagName === 'LI') {
+            ev.target.classList.toggle('checked');
+        }
+    }, false);
+
+    // Create a new list item when clicking on the "Add" button
+    function createChecklistPoint() {
+        var li = document.createElement("li");
+        var inputValue = document.getElementById("taskChecklistInput").value;
+        var t = document.createTextNode(inputValue);
+        li.appendChild(t);
+        if(inputValue === ''){
+            alert("You must write something!");
+        }else {
+            document.getElementById("taskChecklist").appendChild(li);
+        }
+        document.getElementById("taskChecklistInput").value = "";
+        
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+        
+        for(i=0; i < close.length; i++) {
+            close[i].onclick = function(){
+                var div = this.parentElement;
+                div.style.display = "none";
+            }
+        }
+    }
+
+    
+    
+    
+    
+
