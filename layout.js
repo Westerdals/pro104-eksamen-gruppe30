46386353625d538e2f3d523214560def463d5e05
Petter Wibstad <petter.wibstad@icloud.com>
2020-05-21 15:42:31 +0200

    commit

--- layout.js
+++ layout.js
@@ -1,7 +1,12 @@
-function revealAddTaskSection() {
-    var addTaskSection = document.getElementById("addTaskSection");
-    var addTaskRevealBtn = document.getElementById("addTaskRevealBtn");
+/*
 
+This JavaScript file contains code which handles the functionality of the layout.
+
+*/
+
+function revealAddTaskSection() {
+    var addTaskRevealBtn = document.getElementById("addTaskRevealBtn");
+    var addTaskSection = document.getElementById("addTaskSection");
     if (addTaskSection.style.height == "0px" || addTaskSection.style.height == "") {
         addTaskSection.style.height = "250px";
         addTaskSection.style.opacity = "1";
@@ -13,9 +18,6 @@ function revealAddTaskSection() {
         if (membersSection.style.height != "0px" || membersSection.style.height != "") {
             hideSection(membersSection);
         }
-        if (settingsSection.style.height != "0px" || settingsSection.style.height != "") {
-            hideSection(settingsSection);
-        }
 
     } else {
         hideSection(addTaskSection);
@@ -24,15 +26,10 @@ function revealAddTaskSection() {
 }
 
 function revealAddMemberSection() {
-    var addMemberSection = document.getElementById("addMemberSection");
     var addMemberRevealBtn = document.getElementById("addMemberRevealBtn");
-
+    var addMemberSection = document.getElementById("addMemberSection");
     if (addMemberSection.style.height == "0px" || addMemberSection.style.height == "") {
-<<<<<<< HEAD
-        addMemberSection.style.height = "150px";
-=======
         addMemberSection.style.height = "200px";
->>>>>>> master
         addMemberSection.style.opacity = "1";
         addMemberRevealBtn.style.backgroundColor = "orange";
 
@@ -43,47 +40,17 @@ function revealAddMemberSection() {
             hideSection(membersSection);
         }
 
-        if (settingsSection.style.height != "0px" || settingsSection.style.height != "") {
-            hideSection(settingsSection);
-        }
-
-
     } else {
         hideSection(addMemberSection);
     }
     window.setTimeout(hideAdditionForms, 1000);
 }
 
-function revealSettings() {
-    var settingsSection = document.getElementById("settingsSection");
-    var revealSettingsSectionBtn = document.getElementById("revealSettingsBtn");
-
-    if (settingsSection.style.height == "0px" || settingsSection.style.height == "") {
-        settingsSection.style.height = "150px";
-        settingsSection.style.opacity = "1";
-
-        if (addTaskSection.style.height != "0px" || addTaskSection.style.height != "") {
-            hideSection(addTaskSection);
-        }
-        if (addMemberSection.style.height != "0px" || addMemberSection.style.height != "") {
-            hideSection(addMemberSection);
-        }
-        if (membersSection.style.height != "0px" || membersSection.style.height != "") {
-            hideSection(membersSection);
-        }
-    }
-}
-
 function revealMembersSection() {
-    var membersSection = document.getElementById("membersSection");
     var revealMembersBtn = document.getElementById("revealMembersBtn");
-
+    var membersSection = document.getElementById("membersSection");
     if (membersSection.style.height == "0px" || membersSection.style.height == "") {
-<<<<<<< HEAD
         membersSection.style.height = "110px";
-=======
-        membersSection.style.height = "150px";
->>>>>>> master
         membersSection.style.opacity = "1";
         revealMembersBtn.style.backgroundColor = "orange";
 
@@ -93,9 +60,6 @@ function revealMembersSection() {
         if (addMemberSection.style.height != "0px" || addMemberSection.style.height != "") {
             hideSection(addMemberSection);
         }
-        if (settingsSection.style.height != "0px" || settingsSection.style.height != "") {
-            hideSection(settingsSection);
-        }
 
     } else {
         hideSection(membersSection);
@@ -103,12 +67,9 @@ function revealMembersSection() {
     window.setTimeout(hideAdditionForms, 1000);
 }
 
-
-
 function revealArchive() {
-    var archive = document.getElementById("archive");
     var revealArchiveBtn = document.getElementById("revealArchiveBtn");
-
+    var archive = document.getElementById("archive");
     if (archive.style.height == "0px" || archive.style.height == "") {
         archive.style.height = "630px";
         archive.style.opacity = "1";
@@ -131,84 +92,61 @@ function revealArchive() {
 function revealAddDescriptionForm(){
     var taskDescriptionForm = document.getElementById("taskDescriptionForm");
     var addDescriptionRevealBtn = document.getElementById("addDescriptionRevealBtn");
-    var taskDescription = document.getElementById("taskDescription");
 
     if (taskDescriptionForm.style.height == "0px" || taskDescriptionForm.style.height == ""){
         taskDescriptionForm.style.height = "200px";
         taskDescriptionForm.style.opacity = "1";
         addDescriptionRevealBtn.style.backgroundColor = "orange";
-        taskDescription.style.display = "inline-block";
 
         if (taskChecklistForm.style.height != "0px" || taskChecklistForm.style.height != "") {
             hideSection(taskChecklistForm);
-            hideAdditionInputs(taskChecklistForm);
         }
         if (taskIconForm.style.height != "0px" || taskIconForm.style.height != "") {
             hideSection(taskIconForm);
-            hideAdditionInputs(taskIconForm);
         }
 
     } else {
         hideSection(taskDescriptionForm);
-        hideAdditionInputs(taskDescriptionForm);
     }
 }
 
 function revealAddChecklistForm(){
     var taskChecklistForm = document.getElementById("taskChecklistForm");
-    var addChecklistRevealBtn = document.getElementById("addChecklistRevealBtn");
-    var taskChecklistInput = document.getElementById("taskChecklistInput");
-    var createChecklistBtn = document.getElementById("createChecklistBtn");
 
     if (taskChecklistForm.style.height == "0px" || taskChecklistForm.style.height == ""){
         taskChecklistForm.style.height = "200px";
         taskChecklistForm.style.opacity = "1";
         addChecklistRevealBtn.style.backgroundColor = "orange";
-        taskChecklistInput.style.display = "inline-block";
-        createChecklistBtn.style.display = "inline-block";
 
         if (taskDescriptionForm.style.height != "0px" || taskDescriptionForm.style.height != "") {
             hideSection(taskDescriptionForm);
-            hideAdditionInputs(taskDescriptionForm);
         }
         if (taskIconForm.style.height != "0px" || taskIconForm.style.height != "") {
             hideSection(taskIconForm);
-            hideAdditionInputs(taskIconForm);
         }
 
     } else {
         hideSection(taskChecklistForm);
-        hideAdditionInputs(taskChecklistForm);
     }
 }
 
 function revealAddIconForm(){
     var taskIconForm = document.getElementById("taskIconForm");
-    var addIconRevealBtn = document.getElementById("addIconRevealBtn");
-    var buttonIcons = document.getElementsByClassName("buttonIcon")
 
     if (taskIconForm.style.height == "0px" || taskIconForm.style.height == ""){
         taskIconForm.style.height = "200px";
         taskIconForm.style.opacity = "1";
-
-        for(var i = 0; i < buttonIcons.length; i++){
-            buttonIcons[i].style.display = "inline-block";
-        }
-
         addIconRevealBtn.style.backgroundColor = "orange";
 
         if (taskDescriptionForm.style.height != "0px" || taskDescriptionForm.style.height != "") {
             hideSection(taskDescriptionForm);
-            hideAdditionInputs(taskDescriptionForm);
         }
         if (taskChecklistForm.style.height != "0px" || taskChecklistForm.style.height != "") {
             hideSection(taskChecklistForm);
-            hideAdditionInputs(taskChecklistForm);
         }
 
     } else {
         hideSection(taskIconForm);
-        hideAdditionInputs(taskIconForm);
     }
 }
 
@@ -224,29 +162,20 @@ function expandTask(object){
 function hideSection(section) {
     section.style.height = "0px";
     section.style.opacity = "0";
-
-    switch(section){
-        case addTaskSection:
-            addTaskRevealBtn.style.backgroundColor = "";
-            break;
-        case addMemberSection:
-            addMemberRevealBtn.style.backgroundColor = "";
-            break;
-        case membersSection:
-            revealMembersBtn.style.backgroundColor = "";
-            break;
-        case archive:
-            revealArchiveBtn.style.backgroundColor = "";
-            break;
-        case taskDescriptionForm:
-            addDescriptionRevealBtn.style.backgroundColor = "";
-            break;
-        case taskChecklistForm:
-            addChecklistRevealBtn.style.backgroundColor = "";
-            break;
-        case taskIconForm:
-            addIconRevealBtn.style.backgroundColor = "";
-            break;
+    if (section == addTaskSection) {
+        addTaskRevealBtn.style.backgroundColor = "";
+    } else if (section == addMemberSection) {
+        addMemberRevealBtn.style.backgroundColor = "";
+    } else if (section == membersSection) {
+        revealMembersBtn.style.backgroundColor = "";
+    } else if (section == archive) {
+        revealArchiveBtn.style.backgroundColor = "";
+    } else if (section == taskDescriptionForm){
+        addDescriptionRevealBtn.style.backgroundColor = "";
+    } else if (section == taskChecklistForm){
+        addChecklistRevealBtn.style.backgroundColor = "";
+    } else if (section == taskIconForm){
+        addIconRevealBtn.style.backgroundColor = "";
     }
 }
 
@@ -254,39 +183,4 @@ function hideAdditionForms(){
     hideSection(taskDescriptionForm);
     hideSection(taskChecklistForm);
     hideSection(taskIconForm);
-<<<<<<< HEAD
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> master
-=======
-    hideAdditionInputs(taskDescriptionForm);
-    hideAdditionInputs(taskChecklistForm);
-    hideAdditionInputs(taskIconForm);
-}
-
-function hideAdditionInputs(form){
- 
-    switch(form){
-        case taskDescriptionForm:
-            var taskDescription = document.getElementById("taskDescription");
-            taskDescription.style.display = "none";
-            break;
-
-        case taskChecklistForm:
-            var taskChecklistInput = document.getElementById("taskChecklistInput");
-            var createChecklistBtn = document.getElementById("createChecklistBtn");
-            taskChecklistInput.style.display = "none";
-            createChecklistBtn.style.display = "none"
-            break;
-            
-        case taskIconForm:
-            var buttonIcons = document.getElementsByClassName("buttonIcon")
-            for(var i = 0; i < buttonIcons.length; i++){
-                buttonIcons[i].style.display = "none";
-            }
-            break;
-    }
-}
->>>>>>> master
