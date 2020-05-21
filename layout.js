65a763ac6110d8136a31a825971a1e178613ddde
pbyenstuen <56080700+pbyenstuen@users.noreply.github.com>
2020-05-21 12:54:07 +0200

    Bug fix: Input elements no longer clickable/editable when parent section is closed

--- layout.js
+++ layout.js
@@ -5,8 +5,9 @@ This JavaScript file contains code which handles the functionality of the layout
 */
 
 function revealAddTaskSection() {
-    var addTaskRevealBtn = document.getElementById("addTaskRevealBtn");
     var addTaskSection = document.getElementById("addTaskSection");
+    var addTaskRevealBtn = document.getElementById("addTaskRevealBtn");
+
     if (addTaskSection.style.height == "0px" || addTaskSection.style.height == "") {
         addTaskSection.style.height = "250px";
         addTaskSection.style.opacity = "1";
@@ -26,8 +27,9 @@ function revealAddTaskSection() {
 }
 
 function revealAddMemberSection() {
-    var addMemberRevealBtn = document.getElementById("addMemberRevealBtn");
     var addMemberSection = document.getElementById("addMemberSection");
+    var addMemberRevealBtn = document.getElementById("addMemberRevealBtn");
+
     if (addMemberSection.style.height == "0px" || addMemberSection.style.height == "") {
         addMemberSection.style.height = "200px";
         addMemberSection.style.opacity = "1";
@@ -47,8 +49,9 @@ function revealAddMemberSection() {
 }
 
 function revealMembersSection() {
-    var revealMembersBtn = document.getElementById("revealMembersBtn");
     var membersSection = document.getElementById("membersSection");
+    var revealMembersBtn = document.getElementById("revealMembersBtn");
+
     if (membersSection.style.height == "0px" || membersSection.style.height == "") {
         membersSection.style.height = "150px";
         membersSection.style.opacity = "1";
@@ -68,8 +71,9 @@ function revealMembersSection() {
 }
 
 function revealArchive() {
-    var revealArchiveBtn = document.getElementById("revealArchiveBtn");
     var archive = document.getElementById("archive");
+    var revealArchiveBtn = document.getElementById("revealArchiveBtn");
+
     if (archive.style.height == "0px" || archive.style.height == "") {
         archive.style.height = "630px";
         archive.style.opacity = "1";
@@ -92,61 +96,84 @@ function revealArchive() {
 function revealAddDescriptionForm(){
     var taskDescriptionForm = document.getElementById("taskDescriptionForm");
     var addDescriptionRevealBtn = document.getElementById("addDescriptionRevealBtn");
+    var taskDescription = document.getElementById("taskDescription");
 
     if (taskDescriptionForm.style.height == "0px" || taskDescriptionForm.style.height == ""){
         taskDescriptionForm.style.height = "200px";
         taskDescriptionForm.style.opacity = "1";
         addDescriptionRevealBtn.style.backgroundColor = "orange";
+        taskDescription.style.display = "inline-block";
 
         if (taskChecklistForm.style.height != "0px" || taskChecklistForm.style.height != "") {
             hideSection(taskChecklistForm);
+            hideAdditionInputs(taskChecklistForm);
         }
         if (taskIconForm.style.height != "0px" || taskIconForm.style.height != "") {
             hideSection(taskIconForm);
+            hideAdditionInputs(taskIconForm);
         }
 
     } else {
         hideSection(taskDescriptionForm);
+        hideAdditionInputs(taskDescriptionForm);
     }
 }
 
 function revealAddChecklistForm(){
     var taskChecklistForm = document.getElementById("taskChecklistForm");
+    var addChecklistRevealBtn = document.getElementById("addChecklistRevealBtn");
+    var taskChecklistInput = document.getElementById("taskChecklistInput");
+    var createChecklistBtn = document.getElementById("createChecklistBtn");
 
     if (taskChecklistForm.style.height == "0px" || taskChecklistForm.style.height == ""){
         taskChecklistForm.style.height = "200px";
         taskChecklistForm.style.opacity = "1";
         addChecklistRevealBtn.style.backgroundColor = "orange";
+        taskChecklistInput.style.display = "inline-block";
+        createChecklistBtn.style.display = "inline-block";
 
         if (taskDescriptionForm.style.height != "0px" || taskDescriptionForm.style.height != "") {
             hideSection(taskDescriptionForm);
+            hideAdditionInputs(taskDescriptionForm);
         }
         if (taskIconForm.style.height != "0px" || taskIconForm.style.height != "") {
             hideSection(taskIconForm);
+            hideAdditionInputs(taskIconForm);
         }
 
     } else {
         hideSection(taskChecklistForm);
+        hideAdditionInputs(taskChecklistForm);
     }
 }
 
 function revealAddIconForm(){
     var taskIconForm = document.getElementById("taskIconForm");
+    var addIconRevealBtn = document.getElementById("addIconRevealBtn");
+    var buttonIcons = document.getElementsByClassName("buttonIcon")
 
     if (taskIconForm.style.height == "0px" || taskIconForm.style.height == ""){
         taskIconForm.style.height = "200px";
         taskIconForm.style.opacity = "1";
+
+        for(var i = 0; i < buttonIcons.length; i++){
+            buttonIcons[i].style.display = "inline-block";
+        }
+
         addIconRevealBtn.style.backgroundColor = "orange";
 
         if (taskDescriptionForm.style.height != "0px" || taskDescriptionForm.style.height != "") {
             hideSection(taskDescriptionForm);
+            hideAdditionInputs(taskDescriptionForm);
         }
         if (taskChecklistForm.style.height != "0px" || taskChecklistForm.style.height != "") {
             hideSection(taskChecklistForm);
+            hideAdditionInputs(taskChecklistForm);
         }
 
     } else {
         hideSection(taskIconForm);
+        hideAdditionInputs(taskIconForm);
     }
 }
 
@@ -162,20 +189,29 @@ function expandTask(object){
 function hideSection(section) {
     section.style.height = "0px";
     section.style.opacity = "0";
-    if (section == addTaskSection) {
-        addTaskRevealBtn.style.backgroundColor = "";
-    } else if (section == addMemberSection) {
-        addMemberRevealBtn.style.backgroundColor = "";
-    } else if (section == membersSection) {
-        revealMembersBtn.style.backgroundColor = "";
-    } else if (section == archive) {
-        revealArchiveBtn.style.backgroundColor = "";
-    } else if (section == taskDescriptionForm){
-        addDescriptionRevealBtn.style.backgroundColor = "";
-    } else if (section == taskChecklistForm){
-        addChecklistRevealBtn.style.backgroundColor = "";
-    } else if (section == taskIconForm){
-        addIconRevealBtn.style.backgroundColor = "";
+
+    switch(section){
+        case addTaskSection:
+            addTaskRevealBtn.style.backgroundColor = "";
+            break;
+        case addMemberSection:
+            addMemberRevealBtn.style.backgroundColor = "";
+            break;
+        case membersSection:
+            revealMembersBtn.style.backgroundColor = "";
+            break;
+        case archive:
+            revealArchiveBtn.style.backgroundColor = "";
+            break;
+        case taskDescriptionForm:
+            addDescriptionRevealBtn.style.backgroundColor = "";
+            break;
+        case taskChecklistForm:
+            addChecklistRevealBtn.style.backgroundColor = "";
+            break;
+        case taskIconForm:
+            addIconRevealBtn.style.backgroundColor = "";
+            break;
     }
 }
 
@@ -183,4 +219,29 @@ function hideAdditionForms(){
     hideSection(taskDescriptionForm);
     hideSection(taskChecklistForm);
     hideSection(taskIconForm);
+    hideAdditionInputs(taskDescriptionForm);
+    hideAdditionInputs(taskChecklistForm);
+    hideAdditionInputs(taskIconForm);
+}
+
+function hideAdditionInputs(form){
+ 
+    switch(form){
+        case taskDescriptionForm:
+            var taskDescription = document.getElementById("taskDescription");
+            taskDescription.style.display = "none";
+            break;
+        case taskChecklistForm:
+            var taskChecklistInput = document.getElementById("taskChecklistInput");
+            var createChecklistBtn = document.getElementById("createChecklistBtn");
+            taskChecklistInput.style.display = "none";
+            createChecklistBtn.style.display = "none"
+            break;
+        case taskIconForm:
+            var buttonIcons = document.getElementsByClassName("buttonIcon")
+            for(var i = 0; i < buttonIcons.length; i++){
+                buttonIcons[i].style.display = "none";
+            }
+            break;
+    }
 }
