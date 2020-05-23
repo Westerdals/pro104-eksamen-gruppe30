bf5935903055248526ab21551105ee0a2bd8c92c
Kulwinder Singh Toor <kulwinder@moscowmail.com>
2020-05-23 03:29:30 +0200

    Update task-manager.js

--- task-manager.js
+++ task-manager.js
@@ -1,69 +1,81 @@
-function revealAddTaskSection() {
-    var addTaskRevealBtn = document.getElementById("addTaskRevealBtn");
-    var addTaskSection = document.getElementById("addTaskSection");
-    if (addTaskSection.style.height == "0px" || addTaskSection.style.height == "") {
-        addTaskSection.style.height = "250px";
-        addTaskSection.style.opacity = "1";
-        addTaskRevealBtn.style.backgroundColor = "lightgray";
+var textSizeDescription = "";
+var textSizeHeader = "";
 
-        if (addMemberSection.style.height != "0px" || addMemberSection.style.height != "") {
-            resetSection(addMemberSection);
-        }
-    } else {
-        addTaskSection.style.height = "0px";
-        addTaskSection.style.opacity = "0";
-        addTaskRevealBtn.style.backgroundColor = "";
-    }
-  }
-
-  function revealAddMemberSection() {
-    var addMemberRevealBtn = document.getElementById("addMemberRevealBtn");
-    var addMemberSection = document.getElementById("addMemberSection");
-    if (addMemberSection.style.height == "0px" || addMemberSection.style.height == "") {
-        addMemberSection.style.height = "150px";
-        addMemberSection.style.opacity = "1";
-        addMemberRevealBtn.style.backgroundColor = "lightgray";
-
-        if (addTaskSection.style.height != "0px" || addTaskSection.style.height != "") {
-            resetSection(addTaskSection);
-        }
+function loadSettings (){
+    if (window.localStorage.length == 0) {
+        console.log("Standard settings loaded");
+        var savedTextSize = "medium";
+        var savedTextSizeHeader = "large" ;
+        var textSize = {savedTextSize, savedTextSizeHeader}
+        window.localStorage.setItem('settings', JSON.stringify(textSize));
         
-    } else {
-        addMemberSection.style.height = "0px";
-        addMemberSection.style.opacity = "0";
-        addMemberRevealBtn.style.backgroundColor = "";
     }
-  }
+    var savedSettings = JSON.parse(localStorage.getItem('settings')) || [];
+    textSizeDescription = savedSettings.savedTextSize;
+    textSizeHeader = savedSettings.savedTextSizeHeader;
+}
 
-  function resetSection(section){
-    section.style.height = "0px";
-    section.style.opacity = "0";
-    if (section == addTaskSection){
-        addTaskRevealBtn.style.backgroundColor = "";
-    } else {
-        addMemberRevealBtn.style.backgroundColor = "";
-    }
-  }
+function adjustText(header, description) {
+    textSizeDescription = description;
+    textSizeHeader = header;
+    var savedSettings = JSON.parse(localStorage.getItem('settings')) || [];
+    var savedTextSize = description;
+    var savedTextSizeHeader = header;
+    var textSize = {savedTextSize, savedTextSizeHeader}
+    window.localStorage.setItem('settings', JSON.stringify(textSize));
 
-  function createTask(event) {
+    renderTaskList();
+}
+
+function createTask(event) {
     event.preventDefault();
+    document.getElementById("currentCount").innerHTML = "0 ";
 
+    const taskList = JSON.parse(localStorage.getItem('task')) || [];
     const taskName = document.querySelector("[name='taskName']").value;
     const taskDescription = document.querySelector("[name='taskDescription']").value;
+    const taskIcon = document.getElementById("pickedIcon").src;
+    
+    /*var taskId = 0;
+    if (taskList.length != 0){
+        taskId=taskList[taskList.length-1].taskId + 1;
+    }*/
 
-    const task = {taskName, taskDescription};
-    const taskList = JSON.parse(localStorage.getItem('task')) || [];
+    let taskId = taskList.length;
+
+    const task = { taskId, taskName, taskDescription, taskIcon };
     taskList.push(task);
 
     window.localStorage.setItem('task', JSON.stringify(taskList));
 
     event.target.reset();
-    resetSection(addTaskSection);
+    hideSection(addTaskSection);
     renderTaskList();
 }
 
+function createMember(event) {
+    event.preventDefault();
+
+    const memberName = document.querySelector("[name='memberName']").value;
+    const memberList = JSON.parse(localStorage.getItem('member')) || [];
+
+    let memberId = memberList.length;
+
+    const member = {memberId, memberName };
+    
+    memberList.push(member);
+
+    window.localStorage.setItem('member', JSON.stringify(memberList));
+
+    event.target.reset();
+    revealMembersSection();
+    renderMemberList();
+}
+
+
 function renderTaskList() {
 
+    document.getElementById("pickedIcon").src="icons/default.png";
     const taskList = JSON.parse(window.localStorage.getItem("task")) || [];
     const unstartedTasks = document.getElementById("unstartedTasks");
 
@@ -71,123 +83,246 @@ function renderTaskList() {
 
     for (const task of taskList) {
         const taskElement = document.createElement("div");
-        const {taskName, taskDescription} = task;
+        const { taskId, taskName, taskDescription, taskIcon } = task;
 
-        taskElement.innerHTML = `<div class="taskObject">
-                                <h4>${task.taskName.charAt(0).toUpperCase() + task.taskName.slice(1)}</h4>
-                                <div>${task.taskDescription}</div>
+        taskElement.innerHTML = `<div id="${taskId}" class="taskObject" onclick="expandTask(this)"
+                                draggable="true" ondragstart="drag(event)" ondragover="allowMoveNames(event)">
+                                <img id="taskIcon" src="${taskIcon}">
+                                <div id="taskHeading"><h4 style="font-size: ${textSizeHeader}; class="adjustHeader">${taskName.charAt(0).toUpperCase() + taskName.slice(1)}</h4></div>
+                                <p style="font-size: ${textSizeDescription};" id="taskDescriptionPara" class="adjustText">${taskDescription}</p>
+                                <button id="deleteTaskBtn" type="button" onclick="deleteTask(${taskId})"><img src="images/trashcan.png" id="trashcan" style="height:30px;" alt="delete task"></button>
+                                <div id="droppedMember" class="droppedMember" 
+                                 ondrop="dropNames(event)">Members</div>
+                                </div>
                                 </div>`;
         unstartedTasks.appendChild(taskElement);
-    }}
-
-
-
-
-    var clock = document.querySelector('#utility-clock')
-utilityClock(clock)
-autoResize(clock, 295 + 32)
-
-choose(clock, [
-  ['hour', ['text', 'text-quarters', 'pill']],
-  ['hour-text', ['large', 'small']],
-  ['hour-display', ['all', 'quarters', 'none']],
-  ['minute', ['line', 'dot']],
-  ['minute-display', ['fine', 'fine-2', 'coarse', 'major', 'none']],
-  ['minute-text', ['inside', 'outside', 'none']],
-  ['hand', ['normal', 'hollow']]
-])
-
-function utilityClock(container) {
-  
-  var dynamic = container.querySelector('.dynamic')
-  var hourElement = container.querySelector('.hour')
-  var minuteElement = container.querySelector('.minute')
-  var secondElement = container.querySelector('.second')
-  
-  var div = function(className, innerHTML) {
-    var element = document.createElement('div')
-    element.className = className
-    element.innerHTML = innerHTML || ''
-    return element
-  }
-
-  var append = function(element) {
-    return {
-      to: function(parent) {
-        parent.appendChild(element)
-        return append(parent)
-      }
     }
-  }
+}
 
-  var anchor = function(element, rotation) {
-    var anchor = div('anchor')
-    rotate(anchor, rotation)
-    append(element).to(anchor).to(dynamic)
-  }
-  
-  var minute = function(n) {
-    var klass = n % 5 == 0 ? 'major' : n % 1 == 0 ? 'whole' : 'part'
-    var line = div('element minute-line ' + klass)
-    anchor(line, n)
-    if (n % 5 == 0) {
-      var text = div('anchor minute-text ' + klass)
-      var content = div('expand content', (n < 10 ? '0' : '') + n)
-      append(content).to(text)
-      rotate(text, -n)
-      anchor(text, n)
-    }
-  }
+function deleteTask(taskId) {
+    var taskList = JSON.parse(window.localStorage.getItem("task")) || [];
+	for(var i = 0; i < taskList.length; i++){
+		if(taskList[i].taskId == taskId){
+			taskList.splice(i, 1);
+		}
+	}
+	window.localStorage.setItem("task", JSON.stringify(taskList));
+	renderTaskList();
+}
 
-  var hour = function(n) {
-    var klass = 'hour-item hour-' + n
-    var line = div('element hour-pill ' + klass)
-    anchor(line, n * 5)
-    var text = div('anchor hour-text ' + klass)
-    var content = div('expand content', n)
-    append(content).to(text)
-    rotate(text, -n * 5)
-    anchor(text, n * 5)
-    return
-  }
+function renderTaskOngoingList() {
 
-  var position = function(element, phase, r) {
-    var theta = phase * 2 * Math.PI
-    element.style.top = (-r * Math.cos(theta)).toFixed(1) + 'px'
-    element.style.left = (r * Math.sin(theta)).toFixed(1) + 'px'
-  }
 
-  var rotate = function(element, second) {
-    element.style.transform = element.style.webkitTransform = 'rotate(' + (second * 6) + 'deg)'
-  }
+    document.getElementById("pickedIcon").src="icons/default.png";
+    const lists = JSON.parse(window.localStorage.getItem("lists")) || [];
+    const ongoingTasks = document.getElementById("ongoingTasks");
 
-  var animate = function() {
-    var now = new Date()
-    var time = now.getHours() * 3600 +
-                now.getMinutes() * 60 +
-                now.getSeconds() * 1 +
-                now.getMilliseconds() / 1000
-    rotate(secondElement, time)
-    rotate(minuteElement, time / 60)
-    rotate(hourElement, time / 60 / 12)
-    requestAnimationFrame(animate)
-  }
 
-  for (var i = 1 / 4; i <= 60; i += 1 / 4) minute(i)
-  for (var i = 1; i <= 12; i ++) hour(i)
+    ongoingTasks.innerHTML = "";
 
-  animate()
+        for(const task of lists) {
+            const taskElement = document.createElement("div");
+            const { taskId, taskName, taskDescription, taskIcon } = task;
+
+            taskElement.innerHTML = `<div id="${taskId}" class="taskObject" onclick="expandTask(this)"
+                                    draggable="true" ondragstart="drag(event)" ondragover="allowMoveNames(event)">
+                                    <img id="taskIcon" src="${taskIcon}">
+                                    <div id="taskHeading"><h4 style="font-size: ${textSizeHeader}; class="adjustHeader">${taskName.charAt(0).toUpperCase() + taskName.slice(1)}</h4></div>
+                                    <p style="font-size: ${textSizeDescription};" id="taskDescriptionPara" class="adjustText">${taskDescription}</p>
+                                    <button id="deleteTaskBtn" type="button" onclick="deleteTaskOngoing(${taskId})"><img src="images/trashcan.png" id="trashcan" style="height:30px;" alt="delete task"></button>
+                                    <div id="droppedMember" class="droppedMember" 
+                                    ondrop="dropNames(event)">Members</div>
+                                    </div>
+                                    </div>`;
+            ongoingTasks.appendChild(taskElement);
+        }
 
 }
 
-function autoResize(element, nativeSize) {
-  var update = function() {
-    var parent = element.offsetParent
-    var scale = Math.min(parent.offsetWidth, parent.offsetHeight) / nativeSize
-    element.style.transform = element.style.webkitTransform = 'scale(' + scale.toFixed(3) + ')'
+function deleteTaskOngoing(taskId) {
+    var lists = JSON.parse(window.localStorage.getItem("lists")) || [];
+  for(var i = 0; i < lists.length; i++){
+    if(lists[i].taskId == taskId){
+      lists.splice(i, 1);
+    }
   }
-  update()
-  window.addEventListener('resize', update)
+    window.localStorage.setItem("lists", JSON.stringify(lists));
+    renderTaskOngoingList();
+  }
+
+function renderTaskFinishedList() {
+
+    document.getElementById("pickedIcon").src="icons/default.png";
+    const fList = JSON.parse(window.localStorage.getItem("fList")) || [];
+    const finishedTasks = document.getElementById("finishedTasks");
+
+    finishedTasks.innerHTML = "";
+
+    for (const task of fList) {
+        const taskElement = document.createElement("div");
+        const { taskId, taskName, taskDescription, taskIcon } = task;
+
+        taskElement.innerHTML = `<div id="${taskId}" class="taskObject" onclick="expandTask(this)"
+                                draggable="true" ondragstart="drag(event)" ondragover="allowMoveNames(event)">
+                                <img id="taskIcon" src="${taskIcon}">
+                                <div id="taskHeading"><h4 style="font-size: ${textSizeHeader}; class="adjustHeader">${taskName.charAt(0).toUpperCase() + taskName.slice(1)}</h4></div>
+                                <p style="font-size: ${textSizeDescription};" id="taskDescriptionPara" class="adjustText">${taskDescription}</p>
+                                <button id="deleteTaskBtn" type="button" onclick="archiveTask(${taskId})"><img src="images/trashcan.png" id="trashcan" style="height:30px;" alt="delete task"></button>
+                                <div id="droppedMember" class="droppedMember" 
+                                 ondrop="dropNames(event)">Members</div>
+                                </div>
+                                </div>`;
+        finishedTasks.appendChild(taskElement);
+    }
 }
 
-fu
+function deleteTaskFinished(taskId) {
+    var fList = JSON.parse(window.localStorage.getItem("fList")) || [];
+  for(var i = 0; i < fList.length; i++){
+    if(fList[i].taskId == taskId){
+      fList.splice(i, 1);
+    }
+  }
+    window.localStorage.setItem("fList", JSON.stringify(fList));
+    renderTaskFinishedList();
+  }
+
+function renderArchiveList(){
+    const archiveList = JSON.parse(localStorage.getItem("archive")) || [];
+
+    let archive = document.getElementById("archive");
+
+    archive.innerHTML = "";
+
+    for(const ar of archiveList){
+        let archiveElement = document.createElement("div");
+        
+        const { taskId, taskName, taskDescription, taskIcon } = ar;
+
+        archiveElement.innerHTML = `<div id="${taskId}" class="taskObject" onclick="expandTask(this)"
+                                    draggable="true" ondragstart="drag(event)" ondragover="allowMoveNames(event)">
+                                    <img id="taskIcon" src="${taskIcon}">
+                                    <div id="taskHeading"><h4 style="font-size: ${textSizeHeader}; class="adjustHeader">${taskName.charAt(0).toUpperCase() + taskName.slice(1)}</h4></div>
+                                    <p style="font-size: ${textSizeDescription};" id="taskDescriptionPara" class="adjustText">${taskDescription}</p>
+                                    <button id="deleteTaskBtn" type="button" onclick="deleteTaskFromArchive(${taskId})"><img src="images/trashcan.png" id="trashcan" style="height:30px;" alt="delete task"></button>
+                                    <div id="droppedMember" class="droppedMember" 
+                                    ondrop="dropNames(event)">Members</div>
+                                    </div>
+                                    </div>`;
+        archive.appendChild(archiveElement);
+    }
+}
+
+function deleteTaskFromArchive(taskId){
+    const archiveList = JSON.parse(window.localStorage.getItem("archive")) || [];
+  for(var i = 0; i < archiveList.length; i++){
+    if(archiveList[i].taskId == taskId){
+      archiveList.splice(i, 1);
+    }
+  }
+    window.localStorage.setItem("archive", JSON.stringify(archiveList));
+    renderArchiveList();
+}
+
+function archiveTask(taskId) {
+    const archiveList = JSON.parse(localStorage.getItem("archive")) || [];
+    var fList = JSON.parse(window.localStorage.getItem("fList")) || [];
+	for(var i = 0; i < fList.length; i++){
+		if(fList[i].taskId == taskId){
+			archiveList.push(fList[i]);
+            deleteTaskFinished(taskId);
+		}
+	}
+	window.localStorage.setItem("archive", JSON.stringify(archiveList));
+	renderArchiveList();
+}
+
+function deleteMember(memberId) {
+    const memberSlots = document.getElementsByClassName("memberSlot");
+
+    var memberList = JSON.parse(window.localStorage.getItem("member")) || [];
+    console.log(memberList);
+    for(var i = 0; i < memberList.length; i++) {
+        if(memberList[i].memberId == memberId) {
+            memberList.splice(i,1);
+        }
+    }
+    window.localStorage.setItem("member", JSON.stringify(memberList));
+    renderMemberList();
+
+    memberSlots[memberId].style.border = "1px dashed gray";
+    memberSlots[memberId].style.backgroundColor = "";
+}
+
+function renderMemberList() {
+
+    const memberList = JSON.parse(window.localStorage.getItem("member")) || [];
+    const memberSlots = document.getElementsByClassName("memberSlot");
+    var counter = -1;
+
+    for (var i = 0; i < memberSlots.length; i++){ 
+        memberSlots[i].innerHTML = "";
+    }
+        for (const member of memberList) {
+
+            const memberElement = document.createElement("div");
+            const {memberId, memberName } = member;
+            counter++;
+            memberElement.innerHTML = `<div class="memberObject" id="${memberId}" 
+                                      draggable="true" ondragstart="dragStartNames(event)">
+                                      <button id="deleteMemberBtn" type="button" onclick="deleteMember('${memberId}')"><img src="images/trashcan.png" id="trashcan" style="height:20px;" alt="delete task"></button>
+                                      <img src="images/member.png" alt="member" width="45" height="45">
+                                      <h4>${memberName}</h4>
+                                      </div>`; 
+            memberSlots[counter].appendChild(memberElement);
+            memberSlots[counter].style.border = "1px solid gray";
+            memberSlots[counter].style.backgroundColor = "#f7fafa";
+    }
+}
+
+function createChecklistPoint() {
+
+    var li = document.createElement("li");
+    var checklistInput = document.getElementById("taskChecklistInput").value;
+    var node = document.createTextNode(checklistInput);
+    li.appendChild(node);
+
+    document.getElementById("taskChecklist").appendChild(li);
+    document.getElementById("taskChecklistInput").value = "";
+}
+
+function createMemberSlots(){
+
+    for(var i = 0; i < 11; i++){
+        var memberSlot = document.createElement("div");
+        memberSlot.setAttribute('class', 'memberSlot');
+        membersSection.appendChild(memberSlot);
+    }
+    renderMemberList();
+}
+
+function createIconButtons(){
+    var icons = ["airplane", "android", "bag", "bbq", "bike", "book", "brush", "building", "cake", "camera", "car", "clock", "cloud",
+     "color-palette", "comment", "cutlery", "desktop-computer", "diagram", "drink", "earth", "envelope", "exclamation-mark", "film",
+     "fitness", "flower", "game-controller", "health", "heart", "house", "laptop", "large-brush", "light-bulb", "mic", "moon", "motorcycle", "mug",
+     "music-note", "paper-clip", "pet", "phone", "run", "school", "shopping-cart", "smartphone", "star", "store", "swim",
+     "trophy", "truck", "wrench"];
+
+    for(var i = 0; i < icons.length; i++){
+        let button = document.createElement("button");
+        button.setAttribute('type', 'button');
+        button.setAttribute('class', 'buttonIcon');
+        button.setAttribute('id', `${icons[i]}Btn`);
+        button.setAttribute('onclick', `document.getElementById("pickedIcon").src = "icons/${icons[i]}.png"`);
+        let img = document.createElement("img");
+        img.setAttribute('src', `icons/${icons[i]}.png`);
+        img.setAttribute('class', 'imageIcon');
+        img.setAttribute('alt', `${icons[i]}`);
+        button.appendChild(img)
+        taskIconForm.appendChild(button);
+    }
+}
+
+function countCharacters(){
+    document.getElementById("currentCount").innerHTML = taskDescription.value.length;
+}
