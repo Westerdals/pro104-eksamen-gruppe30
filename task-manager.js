e6ff636381f1aa44d69ff12caa81e2b8e20e653d
pbyenstuen <56080700+pbyenstuen@users.noreply.github.com>
2020-05-24 22:09:33 +0200

    Small task layout edit

--- task-manager.js
+++ task-manager.js
@@ -133,12 +133,12 @@ function renderTaskList() {
                                     draggable="true" ondragstart="drag(event)" ondragover="allowMoveNames(event)" ondragleave="dragLeave(event)" ondrop="dropNames(event)">
                                     <img id="taskIcon" src="${taskIcon}">
                                     <div id="taskHeading"><h4 contentEditable="true" oninput="changeName(${taskId}, event, 'task');" style="font-size: ${textSizeHeader}; class="adjustHeader">${taskName.charAt(0).toUpperCase() + taskName.slice(1)}</h4></div>
-                                    <div id="progressBarDiv"style="width: 379px; height: 20px; border: 1px solid lightgray;">
+                                    <div id="progressBarDiv">
                                     <div id="progressBar" style="max-width: 380px; width: ${taskProgress}%; height: 20px; background-color: lightgreen;"></div></div>
                                     <div id="checkList">${taskChecklistDiv}</div>
                                     <p style="font-size: ${textSizeDescription};" contentEditable="true" oninput="changeDescription(${taskId}, event, 'ongoingTask');" class="taskDescriptionPara adjustText">${taskDescription}</p>
                                     <button id="deleteTaskBtn" type="button" onclick="deleteTask(${taskId},'task')" onmouseover="this.firstChild.src = 'images/filled-trashcan.png'" onmouseout="this.firstChild.src = 'images/trashcan.png'"><img src="images/trashcan.png" id="trashcan" style="height:30px;" alt="delete task"></button>
-                                    <div>${memberName}</div>
+                                    <div id="taskMembers">${memberName}</div>
                                     </div>
                                     </div>`;
             unstartedTasks.appendChild(taskElement);
