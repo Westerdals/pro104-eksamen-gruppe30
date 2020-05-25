e5a51c5ec4d855f30fe1d40bd163c098af648d05
Petter Wibstad <petter.wibstad@icloud.com>
2020-05-25 17:59:25 +0200

    possible to add multiple members to tasks

--- drag-and-drop.js
+++ drag-and-drop.js
@@ -1,7 +1,7 @@
 /*
 Function takes in the targets id's as parameters, puts the information about these id's and place them into the ongoingList array.
 */
-
+/*
 function moveMembersToTask(memberId, taskId){
   const memberList = JSON.parse(window.localStorage.getItem("member")) || [];
   const taskList = JSON.parse(window.localStorage.getItem("task")) || [];
@@ -22,6 +22,33 @@ function moveMembersToTask(memberId, taskId){
   window.localStorage.setItem("task", JSON.stringify(taskList));
   renderTaskList();
 }
+
+*/
+
+function moveMembersToTask(memberId, taskId, localStorageKey) {
+  console.log(localStorageKey);
+  var taskList = JSON.parse(window.localStorage.getItem(localStorageKey)) || [];
+  console.log(taskList);
+  var memberList = JSON.parse(window.localStorage.getItem("member")) || [];
+  for(var i = 0; i < memberList.length; i++) {
+    if(memberList[i].memberId == memberId) {
+      for(var l = 0; l < taskList.length; l++) {
+        if(taskList[l].taskId == taskId) {
+          const assignedMember = {
+            memberName : memberList[i].memberName,
+            memberId : memberList[i].memberId
+            }
+          taskList[l].taskMembers.push(assignedMember);
+          window.localStorage.setItem(localStorageKey, JSON.stringify(taskList));     
+      }
+    }
+  }
+}
+}
+
+
+
+
 
 /*
 Function takes in three parameters, taskId, localstoragekey from the localstorage you want to retrieve information from,
@@ -49,20 +76,20 @@ function dragStartNames(ev){
     ev.dataTransfer.setData("text/plain", nameDrag);
 }
 
-function dropNames(ev){
-    ev.preventDefault();
+function dropNames(event, localStorageKey){
+    event.preventDefault();
 
-    let memberId = ev.dataTransfer.getData("text/plain");
+    let memberId = event.dataTransfer.getData("text/plain");
 
-    let taskId = ev.target.parentElement.id;
+    let taskId = event.target.parentElement.id;
+    console.log(localStorageKey);
 
-    moveMembersToTask(memberId, taskId);
+    moveMembersToTask(memberId, taskId, localStorageKey);
     
 }
 
 function dragLeave(ev){
   ev.preventDefault();
-
 }
 
 
